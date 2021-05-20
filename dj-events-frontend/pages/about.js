8ee5b51420f104945dce8b4ce371ad6481ccf117
Thomas Billington <thombillington@gmail.com>
2021-05-20 16:45:41 -0700

    added link to home page

---
+++ dj-events-frontend/pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is an app to find the latest DJ and other musical events.</p>
+            <p>Verson: 1.0.0</p>
+        </div>
+    )
+}
